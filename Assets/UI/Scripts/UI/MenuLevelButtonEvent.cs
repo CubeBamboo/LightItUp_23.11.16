dedd5d67f075c8b8549c78b85630a8613dbd32a5
CubeBamboo <repotak032@163.com>
2023-11-30 01:06:34 +0800

    Delete some file

--- Assets/UI/Scripts/UI/MenuLevelButtonEvent.cs
+++ Assets/UI/Scripts/UI/MenuLevelButtonEvent.cs
@@ -10,7 +10,6 @@ namespace UI
     {
         public void OnPointerClick(PointerEventData eventData)
         {
-            //if (RbUIManager.Instance && transform.Find(Common.Constant.RB_TEXT_NAME) && transform.Find(Common.Constant.RB_TEXT_NAME).TryGetComponent<Rigidbody2D>(out var rb)) //RbUIManager存在，且有RbText子物体，并且这个子物体有刚体组件。 RbUIManager exist , && Have a child Object named "RbText", && "RbText" have rb2D.
             if (RbUIManager.Instance) RbUIManager.EventHappened(name);
         }
     }
